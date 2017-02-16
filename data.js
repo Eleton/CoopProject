6f81e92835503e0e0763c3474d892b373dc22c56
Eleton <eleton@gmail.com>
2017-02-16 10:51:34 +0100

    project uploaded to github

---
+++ data.js
@@ -0,0 +1,45 @@
+var products = [
+	{
+		"id": 1,
+		"name": "ägg",
+		"eko": false,
+		"price": 35.95,
+		"category": "eggs"
+	},
+	{
+		"id": 2,
+		"name": "ägg",
+		"eko": true,
+		"price": 39.95,
+		"category": "eggs"
+	},
+	{
+		"id": 3,
+		"name": "mjölk",
+		"eko": false,
+		"price": 10,
+		"category": "milk"
+	},
+	{
+		"id": 4,
+		"name": "mjölk",
+		"eko": true,
+		"price": 12,
+		"category": "milk"
+	},
+	{
+		"id": 5,
+		"name": "lamm",
+		"eko": false,
+		"price": 40,
+		"category": "meat"
+	},
+	{
+		"id": 6,
+		"name": "lamm",
+		"eko": true,
+		"price": 50,
+		"category": "meat"
+	},
+	
+]
