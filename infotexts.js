75ebcfa1536b46794f3c971f7b9beeb29ef0c108
dezuman <johannes.borg@hotmail.com>
2017-02-17 10:52:02 +0100

    - Konflikthantering

---
+++ infotexts.js
@@ -0,0 +1,5 @@
+var infotexts = [
+		"Detta pajdiagram visar hur stor del av ditt nuvarande matvaruinköp som är ekologiskt, baserat på varornas kostand i kronor. ", 
+		"Här listas ekologiska varor som är på rea just nu, passa på att göra ett miljövänligt klipp! ", 
+		"Ekopanelen är ett projekt med målsättning att via historisk och dynamisk data underlätta för dig att välja miljövänliga alternativ när du handlar mat. "
+];
