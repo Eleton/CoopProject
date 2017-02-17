b2a98f97a360744f4848f18da1e3f72672271df9
Eleton <eleton@gmail.com>
2017-02-17 11:20:32 +0100

    selected product info added

--- data.js
+++ data.js
@@ -4,42 +4,48 @@ var products = [
 		"name": "ägg",
 		"eko": false,
 		"price": 35.95,
-		"category": "eggs"
+		"category": "eggs",
+		"info": "Kravmärkta ägg är helt klart bättre än andra ägg"
 	},
 	{
 		"id": 2,
 		"name": "ägg",
 		"eko": true,
 		"price": 39.95,
-		"category": "eggs"
+		"category": "eggs",
+		"info": "Kravmärkta ägg är helt klart bättre än andra ägg"
 	},
 	{
 		"id": 3,
 		"name": "mjölk",
 		"eko": false,
 		"price": 10,
-		"category": "milk"
+		"category": "milk",
+		"info": "Om mjölken är ekologisk så betyder det att korna har det mycket bra och sånt"
 	},
 	{
 		"id": 4,
 		"name": "mjölk",
 		"eko": true,
 		"price": 12,
-		"category": "milk"
+		"category": "milk",
+		"info": "Om mjölken är ekologisk så betyder det att korna har det mycket bra och sånt"
 	},
 	{
 		"id": 5,
 		"name": "lamm",
 		"eko": false,
 		"price": 40,
-		"category": "meat"
+		"category": "meat",
+		"info": "Det antibiotikafria ekoköttet är det finaste vi har i Sverige och vi bör värna om det"
 	},
 	{
 		"id": 6,
 		"name": "lamm",
 		"eko": true,
 		"price": 50,
-		"category": "meat"
+		"category": "meat",
+		"info": "Det antibiotikafria ekoköttet är det finaste vi har i Sverige och vi bör värna om det"
 	},
 	
 ]
